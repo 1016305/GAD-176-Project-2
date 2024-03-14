472a971592c84dd3d237d063c20a3dea0a68ab98
1016305 <151628040+1016305@users.noreply.github.com>
2024-03-14 20:39:43 +0800

    Progress on player controlled
    Additional suffering included

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -16,12 +16,17 @@ namespace SAE.GAD176.Project2
         private Vector3 movementVector;
         private Vector3 mouseWorldPosition;
         private Camera cam;
+        protected bool attackDelay = false;
+        [SerializeField] private PlayerRayCast playerraycaster;
         #endregion
 
-        #region Serialized Variables
+        #region Serialized Variables, also controlled by items
         [SerializeField] private float moveSpeed = 5000f;
         [SerializeField] private float moveDrag = 10f;
         [SerializeField] private int maxHealth = 100;
+        [SerializeField] private float attackRange = 3;
+        [SerializeField] private float attackDamage = 5;
+        [SerializeField] private int attackDelayTime = 2;
         private int currentHealth;
         #endregion
 
@@ -44,6 +49,7 @@ namespace SAE.GAD176.Project2
         void Update()
         {
             GetInput();
+            playerraycaster.Attack();
         }
 
         private void FixedUpdate()
@@ -66,7 +72,10 @@ namespace SAE.GAD176.Project2
             //  mouse position on screen.
             Vector3 mouseScreenPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, (cam.transform.position.y - transform.position.y));
             mouseWorldPosition = Camera.main.ScreenToWorldPoint(mouseScreenPosition);
+
+
         }
+
         //Movement uses rigidbody addforce. It does not use AddRelativeForce, as the direction the player is looking is
         //  independent from the direction they are moving.
         //Drag is added as an adjustable variable to control how quickly the player will slow down after movement input
@@ -77,17 +86,20 @@ namespace SAE.GAD176.Project2
             rb.drag = moveDrag;
             cam.transform.position = new Vector3(transform.position.x, cam.transform.position.y, transform.position.z);
         }
+
         //RotateToMouse will rotate the player in the direction of the mouse's location on screen. This took me FAR too
         //  long to figure out, so this whole assembly is a big mess. However, it still works.
         private void RotateToMouse()
         {
             transform.LookAt(new Vector3(mouseWorldPosition.x, transform.position.y, mouseWorldPosition.z), Vector3.up);
         }
+
         //Basic setter for other classes to access to damage the player.
         public void TakeDamage(int damage)
         {
             currentHealth -= damage;
         }
+
         //Universal getter. I found a new thing I hate. It's called a tuple. I can use it to return multiple values from
         //  a single method, wherein I can choose which data is output. Summary so I know what values return per item
         //  when I access it in other classes.
@@ -99,6 +111,13 @@ namespace SAE.GAD176.Project2
         {
             return Tuple.Create(maxHealth, currentHealth);
         }
-        #endregion
+
+        //Player has an attack angle of 45 degrees relative to local z+. Can only attack enemies if they are within this
+        //  range. Multiple rays are cast within the 45 degree range. Max length of rays are determined by the attackRange
+        // variable, which can be adjusted by having items.
+        
+        Nicholas listen to me. Use that Vector3.Rotate or whatever its called but instead of the rotate axis bing
+            vector3.up, have the rotate axis be the transform.position. Okay
+            #endregion
+        }
     }
-}
